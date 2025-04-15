c5e4b2017c380cdac8d37307da2d71f798627600
colehanan1 <c.b.hanan@wustl.edu>
2025-04-15 11:53:25 -0500

    Reduce batch size in adaptive_weights.py from 64 to 16 for improved training performance

--- adaptive_weights.py
+++ adaptive_weights.py
@@ -152,7 +152,7 @@ transform = transforms.Compose([
     transforms.ToTensor(),  # MNIST images are 1x28x28
     transforms.Normalize((0.5,), (0.5,))
 ])
-batch_size = 64
+batch_size = 16
 trainset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
 testset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)
